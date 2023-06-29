e5f728dc48a33a0e14a7e14d5bdcd39480ac340c
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-06-29 19:18:48 -0300

    :lipstick: feat: update

--- Profile/src/styles/themes/defaultTheme.ts
+++ Profile/src/styles/themes/defaultTheme.ts
@@ -13,3 +13,16 @@ export const defaultTheme = {
     BEIGE: "#E9E2DA"
   }
 };
+
+const size = {
+  mobile: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  desktop: "2560px"
+};
+export const device = {
+  mobile: `(min-width: ${size.mobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  desktop: `(min-width: ${size.desktop})`
+};
