1f34c3c31eaf2c4a19507ac7cad7953cd626b96c
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-06-30 21:09:20 -0300

    :sparkles: feat: Container and Section

---
+++ Profile/src/components/HTMLElement/Section/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  width: 100%;
+  max-width: 80rem;
+  background-color: red;
+  min-height: 100vh;
+  padding: 2.5rem 1.5rem;
+`;
