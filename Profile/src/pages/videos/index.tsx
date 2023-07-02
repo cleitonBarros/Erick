a02a4be0932f03684762dc89c7017383ec4bae1b
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-07-02 15:10:12 -0300

    :sparkles::lipstick::iphone: feat: video gallery

---
+++ Profile/src/pages/videos/index.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+
+import { Section } from "../../components/HTMLElement/Section/style";
+
+import video1 from "../../assets/video/video1.mp4";
+import video2 from "../../assets/video/video2.mp4";
+import video3 from "../../assets/video/video3.mp4";
+import video4 from "../../assets/video/video4.mp4";
+import * as S from "./style";
+
+export function Videos() {
+  useEffect(() => {
+    const clips = document?.querySelectorAll("video");
+
+    if (clips) {
+      for (let i = 0; i < clips.length; i++) {
+        clips[i].addEventListener("mouseenter", () => {
+          clips[i].play();
+        });
+      }
+
+      for (let i = 0; i < clips.length; i++) {
+        clips[i].addEventListener("mouseout", () => {
+          clips[i].pause();
+        });
+      }
+    }
+  }, []);
+
+  return (
+    <>
+      <Section>
+        <S.Container>
+          <S.Box>
+            <video src={video1} loop></video>
+          </S.Box>
+          <S.Box>
+            <video src={video2} loop></video>
+          </S.Box>
+          <S.Box>
+            <video src={video3} loop></video>
+          </S.Box>
+          <S.Box>
+            <video src={video4} loop></video>
+          </S.Box>
+        </S.Container>
+      </Section>
+    </>
+  );
+}
---
+++ Profile/src/pages/videos/style.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+  margin-top: 2.5rem;
+`;
+export const Box = styled.div`
+  position: relative;
+  filter: grayscale(1);
+  width: 100%;
+  height: 100vh;
+  margin-bottom: 3rem;
+
+  &:hover {
+    filter: grayscale(0);
+  }
+
+  video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  @media ${({ theme }) => theme.DEVICE.min} {
+    video {
+      padding: 2rem;
+    }
+  }
+  @media ${({ theme }) => theme.DEVICE.mobile} {
+    height: 100vh;
+
+    video {
+      padding: 4rem;
+    }
+  }
+  @media ${({ theme }) => theme.DEVICE.laptop} {
+    margin-bottom: 0;
+    height: 100vh;
+
+    video {
+      padding: 6rem;
+    }
+  }
+`;
