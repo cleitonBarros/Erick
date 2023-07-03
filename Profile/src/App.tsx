f0b17da2848394defca1328e1674ea28652ec1c4
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-07-02 21:05:00 -0300

    :fire: feat: deleted some files

--- Profile/src/App.tsx
+++ Profile/src/App.tsx
@@ -1,21 +1,9 @@
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { Container } from "./components/HTMLElement/Container/style";
-import { Slider } from "./components/Slider";
-
-import { Main } from "./pages";
 import { AppRoutes } from "./routes/app.routes";
 
 function App() {
   return (
     <>
       <AppRoutes />
-      {/* <Header />
-      <Slider />
-      <Container>
-        <Main />
-      </Container>
-      <Footer /> */}
     </>
   );
 }
