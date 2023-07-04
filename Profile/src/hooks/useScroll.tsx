80916cc61dade11d956520aaac3af1a1697ee28f
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-07-04 17:42:25 -0300

    :recycle: refactor: type props ={children}

--- Profile/src/hooks/useScroll.tsx
+++ Profile/src/hooks/useScroll.tsx
@@ -1,6 +1,8 @@
 import { PureComponent } from "react";
-
-class ScrollIntoView extends PureComponent<any, any> {
+type Props = {
+  children?: React.ReactNode;
+};
+class ScrollIntoView extends PureComponent<Props> {
   componentDidMount = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
   // eslint-disable-next-line react/prop-types
