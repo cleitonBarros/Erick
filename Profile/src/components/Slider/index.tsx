5be5d0fba9894c12be11aa3dab3f59f68f5bbc60
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-07-03 22:37:34 -0300

    :recycle: refactor: slider

--- Profile/src/components/Slider/index.tsx
+++ Profile/src/components/Slider/index.tsx
@@ -1,27 +1,30 @@
+import Carousel from "react-bootstrap/Carousel";
+
 import queenbee from "../../assets/queenbee.jpg";
 import queenbee2 from "../../assets/queenbeenface.jpg";
 import senna from "../../assets/senna.jpg";
 import wolf from "../../assets/wolfzin.jpg";
 import * as S from "./style";
 
-import { Carousel } from "@trendyol-js/react-carousel";
+//import { Carousel } from "@trendyol-js/react-carousel";
 
 export function Slider() {
   return (
     <div id="Inicio">
       <S.Container>
-        <Carousel
-          className="slider"
-          responsive={true}
-          slide={1}
-          useArrowKeys={true}
-          swiping={true}
-          show={1}
-        >
-          <img src={wolf} alt="lobo colorido" />
-          <img src={queenbee} alt="mulher branca com olhar de brava" />
-          <img src={senna} alt="mulhe negra com capuz" />
-          <img src={queenbee2} alt="mulher branca" />
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block w-100" src={senna} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={queenbee} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={wolf} alt="Third slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={queenbee2} alt="Third slide" />
+          </Carousel.Item>
         </Carousel>
       </S.Container>
     </div>
--- Profile/src/components/Slider/style.ts
+++ Profile/src/components/Slider/style.ts
@@ -1,18 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
+  height: 100vh;
   padding-top: 4.5rem;
-
-  img {
-    width: 100%;
-    height: auto;
-  }
-
   @media ${({ theme }) => theme.DEVICE.laptop} {
     padding-top: 6rem;
   }
