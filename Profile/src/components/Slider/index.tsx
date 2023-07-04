5cef72e8f88041b13bb487dd1e972e4690822688
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-07-04 09:43:16 -0300

    :recycle: refactor: slider

--- Profile/src/components/Slider/index.tsx
+++ Profile/src/components/Slider/index.tsx
@@ -1,12 +1,12 @@
 import Carousel from "react-bootstrap/Carousel";
 
+import cat from "../../assets/cat1 1.png";
+import dog from "../../assets/dog 1.png";
 import queenbee from "../../assets/queenbee.jpg";
 import queenbee2 from "../../assets/queenbeenface.jpg";
 import senna from "../../assets/senna.jpg";
 import wolf from "../../assets/wolfzin.jpg";
 import * as S from "./style";
-
-//import { Carousel } from "@trendyol-js/react-carousel";
 
 export function Slider() {
   return (
@@ -17,7 +17,13 @@ export function Slider() {
             <img className="d-block w-100" src={senna} alt="First slide" />
           </Carousel.Item>
           <Carousel.Item>
+            <img className="d-block w-100" src={dog} alt="Third slide" />
+          </Carousel.Item>
+          <Carousel.Item>
             <img className="d-block w-100" src={queenbee} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={cat} alt="Third slide" />
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100" src={wolf} alt="Third slide" />
