6eca9727983d56b3928bfec5c1f72108c8b55d0c
Cleiton barros <48657479+cleitonBarros@users.noreply.github.com>
2023-07-06 14:19:22 -0300

    :sparkles: feat: util

---
+++ Profile/src/utils/index.tsx
@@ -0,0 +1,223 @@
+import cat1 from "../assets/picture/gallery/cat1.jpg";
+import cat2 from "../assets/picture/gallery/cat2.jpg";
+import cat3 from "../assets/picture/gallery/cat3.jpg";
+import dog1 from "../assets/picture/gallery/dog.jpg";
+import eye1 from "../assets/picture/gallery/eye1.jpg";
+import eye2 from "../assets/picture/gallery/eye2.jpg";
+import eye3 from "../assets/picture/gallery/eye3.jpg";
+import eye4 from "../assets/picture/gallery/eye4.jpg";
+import eye5 from "../assets/picture/gallery/eye5.jpg";
+import forest1 from "../assets/picture/gallery/forest1.jpg";
+import forest2 from "../assets/picture/gallery/forest2.jpg";
+import gambit from "../assets/picture/gallery/gambit.jpg";
+import leblanc1 from "../assets/picture/gallery/leblanc.jpg";
+import leblanc2 from "../assets/picture/gallery/leblanc2.jpg";
+import night1 from "../assets/picture/gallery/night.jpg";
+import night2 from "../assets/picture/gallery/night2.jpg";
+import queen2 from "../assets/picture/gallery/queen2.jpg";
+import senna1 from "../assets/picture/gallery/senna1.jpg";
+import senna2 from "../assets/picture/gallery/senna2.jpg";
+import tutty from "../assets/picture/gallery/tutty.jpg";
+import wolf1 from "../assets/picture/gallery/wolf1.jpg";
+import wolf2 from "../assets/picture/gallery/wolf2.jpg";
+import wolf3 from "../assets/picture/gallery/wolf3.jpg";
+import woman from "../assets/picture/gallery/woman.jpg";
+import cat from "../assets/picture/slider/cat1 1.png";
+import dog from "../assets/picture/slider/dog 1.png";
+import queenbee from "../assets/picture/slider/queenbee.jpg";
+import queenbee2 from "../assets/picture/slider/queenbeenface.jpg";
+import senna from "../assets/picture/slider/senna.jpg";
+import wolf from "../assets/picture/slider/wolfzin.jpg";
+import video1 from "../assets/video/video1.mp4";
+import video10 from "../assets/video/video10.mp4";
+import video2 from "../assets/video/video2.mp4";
+import video3 from "../assets/video/video3.mp4";
+import video4 from "../assets/video/video4.mp4";
+import video5 from "../assets/video/video5.mp4";
+import video6 from "../assets/video/video6.mp4";
+import video7 from "../assets/video/video7.mp4";
+import video8 from "../assets/video/video8.mp4";
+import video9 from "../assets/video/video9.mp4";
+
+export const Slide = [
+  {
+    src: wolf,
+    alt: "ilustração de um lobo, sobre um mesa iluminda e um fileira de lapiz de cores "
+  },
+  {
+    src: senna,
+    alt: "Ilustração da senna, uma personagem do jogo league of legends, mulher negra de capuz"
+  },
+  {
+    src: dog,
+    alt: "Ilustração de um cachorro braco"
+  },
+  {
+    src: queenbee,
+    alt: "ilustração de uma mulher branca e loira"
+  },
+  {
+    src: cat,
+    alt: "ilustração de um mesclado , preto com branco"
+  },
+  {
+    src: queenbee2,
+    alt: "ilustração de uma mulher branca e loira"
+  }
+];
+
+export const Photos = [
+  {
+    id: 1,
+    src: cat1,
+    alt: "ilustrção de um gato "
+  },
+  {
+    id: 3,
+    src: eye1,
+    alt: "ilustrção de um olho com fundo roxo "
+  },
+  {
+    id: 8,
+    src: gambit,
+    alt: "ilustrção da personagem da serie o gambito da rainha "
+  },
+  {
+    id: 25,
+    src: cat3,
+    alt: "ilustrção de um gato "
+  },
+  {
+    id: 2,
+    src: dog1,
+    alt: "ilustrção de um cão "
+  },
+  {
+    id: 4,
+    src: eye2,
+    alt: "ilustrção de um olho com fundo rosado "
+  },
+  {
+    id: 17,
+    src: senna2,
+    alt: "Ilustração da senna, uma personagem do jogo league of legends, mulher negra de capuz"
+  },
+  {
+    id: 6,
+    src: eye4,
+    alt: "ilustrção de um olho com fundo azul "
+  },
+  {
+    id: 15,
+    src: queen2,
+    alt: "ilustração de uma mulher branca e loira"
+  },
+  {
+    id: 7,
+    src: eye5,
+    alt: "ilustrção de um olho verde "
+  },
+  {
+    id: 10,
+    src: forest2,
+    alt: "ilustrção de um floresta radiante, iluminada pelos raios do sol "
+  },
+  {
+    id: 11,
+    src: leblanc1,
+    alt: "ilustrção da lembranca, personagem do jogo league of legends "
+  },
+  {
+    id: 22,
+    src: wolf3,
+    alt: "ilustração de um lobos preto"
+  },
+  {
+    id: 12,
+    src: leblanc2,
+    alt: "ilustrção da lembranca, personagem do jogo league of legends "
+  },
+  {
+    id: 24,
+    src: night1,
+    alt: "ilustrção de uma floresta de noite estrelada "
+  },
+  {
+    id: 13,
+    src: night2,
+    alt: "ilustrção de uma floresta de noite estrelada "
+  },
+  {
+    id: 5,
+    src: eye3,
+    alt: "ilustrção de um olho com fundo alaranjado "
+  },
+  {
+    id: 16,
+    src: senna1,
+    alt: "Ilustração da senna, uma personagem do jogo league of legends, mulher negra de capuz"
+  },
+  {
+    id: 18,
+    src: tutty,
+    alt: "ilustração um cachorro feliz"
+  },
+  {
+    id: 19,
+    src: cat2,
+    alt: "ilustrção de um gato "
+  },
+  {
+    id: 20,
+    src: wolf1,
+    alt: "ilustração de um lobos mostrando os dentes"
+  },
+  {
+    id: 21,
+    src: wolf2,
+    alt: "ilustração de um lobos preto"
+  },
+  {
+    id: 23,
+    src: woman,
+    alt: "ilustração de uma mulher asiatica"
+  },
+  {
+    id: 9,
+    src: forest1,
+    alt: "ilustrção de um floresta radiante, iluminada pelos raios do sol "
+  }
+];
+
+export const videos = [
+  {
+    src: video1
+  },
+  {
+    src: video2
+  },
+  {
+    src: video3
+  },
+  {
+    src: video4
+  },
+  {
+    src: video5
+  },
+  {
+    src: video6
+  },
+  {
+    src: video7
+  },
+  {
+    src: video8
+  },
+  {
+    src: video9
+  },
+  {
+    src: video10
+  }
+];
